8130b87ed1235d61739369daa7ef59c835006c8a
Tai <184213591+Vantai0302-tech@users.noreply.github.com>
2026-08-07 09:17:00 +0700

    Drivers for projects

--- Docs/Drivers/BSP/buzzer.h
+++
@@ -1,7 +0,0 @@
-#ifndef BUZZER_H
-#define BUZZER_H
-
-void Buzzer_On(void);
-void Buzzer_Off(void);
-
-#endif /* BUZZER_H */
--- Docs/Drivers/BSP/oled_i2c.h
+++
@@ -1,8 +0,0 @@
-#ifndef OLED_H
-#define OLED_H
-
-void OLED_Init(void);
-void OLED_ShowLevel(float level);
-void OLED_ShowStatus(const char *st);
-
-#endif /* OLED_H */
