e18544c19ae0ae9dfb0cc1d41d4c53a0ec9b9259
Nhat Nguyen <albert.nguyentechemb@gmail.com>
2026-08-11 22:56:00 +0700

    debug with oled and uart test state

--- Firmware/Core/Src/main.c
+++ Firmware/Core/Src/main.c
@@ -4,18 +4,9 @@
   * @file           : main.c
   * @brief          : Main program body
   ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
   */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "i2c.h"
@@ -23,6 +14,8 @@
 #include "usart.h"
 #include "gpio.h"
 #include "logger.h"
+#include "oled.h"
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -44,15 +37,15 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
 
+/* USER CODE BEGIN PFP */
+void I2C_Scan(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -66,56 +59,129 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
+    /* MCU Configuration------------------------------------------------------*/
 
-  /* USER CODE BEGIN 1 */
+    HAL_Init();
 
-  /* USER CODE END 1 */
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_I2C1_Init();
+    MX_TIM2_Init();
+    MX_TIM3_Init();
+    MX_USART1_UART_Init();
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* LED initially OFF */
+    HAL_GPIO_WritePin(
+        GPIOC,
+        GPIO_PIN_13,
+        GPIO_PIN_RESET
+    );
 
-  /* USER CODE BEGIN Init */
+    /* Initialize logger */
+    Logger_Init();
 
-  /* USER CODE END Init */
+    /* Give peripherals some time to stabilize */
+    HAL_Delay(100);
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* ---------------------------------------------------------------------- */
+    /* I2C SCAN                                                              */
+    /* ---------------------------------------------------------------------- */
 
-  /* USER CODE BEGIN SysInit */
+    Logger_Print("\r\n");
+    Logger_Print("================================\r\n");
+    Logger_Print("       OLED TEST START\r\n");
+    Logger_Print("================================\r\n");
 
-  /* USER CODE END SysInit */
+    I2C_Scan();
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C1_Init();
-  MX_TIM2_Init();
-  MX_TIM3_Init();
-  MX_USART1_UART_Init();
+    /* ---------------------------------------------------------------------- */
+    /* OLED INIT                                                             */
+    /* ---------------------------------------------------------------------- */
 
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-  Logger_Init();
-  /* USER CODE BEGIN 2 */
+    Logger_Print("Initializing OLED...\r\n");
 
+    OLED_Init();
 
-  /* USER CODE END 2 */
+    Logger_Print("OLED Init Done\r\n");
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-    {   
-        float distance = 35.5f;
-        float level = 72.3f;
-        Logger_Printf(
-            "[SENSOR] Distance = %.1f cm | Level = %.1f %%\r\n",
-            distance,
-            level
+    /* ---------------------------------------------------------------------- */
+    /* Initial OLED screen                                                   */
+    /* ---------------------------------------------------------------------- */
+
+    OLED_ShowLevel(65.0f);
+    OLED_ShowStatus("FILLING");
+
+    Logger_Print("OLED Display Test Started\r\n");
+
+    /* Infinite loop --------------------------------------------------------*/
+    while (1)
+    {
+        /* -------------------------------------------------------------- */
+        /* Test 1: FILLING                                               */
+        /* -------------------------------------------------------------- */
+
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+
+        OLED_ShowLevel(35.5f);
+        OLED_ShowStatus("FILLING");
+
+        Logger_Print(
+            "[OLED] Level = 35.5%% | State = FILLING\r\n"
         );
 
-        HAL_Delay(1000);
+        HAL_Delay(2000);
+
+
+        /* -------------------------------------------------------------- */
+        /* Test 2: NORMAL                                                */
+        /* -------------------------------------------------------------- */
+
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+
+        OLED_ShowLevel(65.0f);
+        OLED_ShowStatus("NORMAL");
+
+        Logger_Print(
+            "[OLED] Level = 65.0%% | State = NORMAL\r\n"
+        );
+
+        HAL_Delay(2000);
+
+
+        /* -------------------------------------------------------------- */
+        /* Test 3: FULL                                                  */
+        /* -------------------------------------------------------------- */
+
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+
+        OLED_ShowLevel(85.7f);
+        OLED_ShowStatus("FULL");
+
+        Logger_Print(
+            "[OLED] Level = 85.7%% | State = FULL\r\n"
+        );
+
+        HAL_Delay(2000);
+
+
+        /* -------------------------------------------------------------- */
+        /* Test 4: ERROR                                                 */
+        /* -------------------------------------------------------------- */
+
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+
+        OLED_ShowLevel(-1.0f);
+        OLED_ShowStatus("SENSOR ERR");
+
+        Logger_Print(
+            "[OLED] Level = ERROR | State = SENSOR ERR\r\n"
+        );
+
+        HAL_Delay(2000);
     }
-  /* USER CODE END 3 */
 }
 
 /**
@@ -124,40 +190,68 @@ int main(void)
   */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    RCC_ClkInitStruct.ClockType =
+        RCC_CLOCKTYPE_HCLK |
+        RCC_CLOCKTYPE_SYSCLK |
+        RCC_CLOCKTYPE_PCLK1 |
+        RCC_CLOCKTYPE_PCLK2;
+
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(
+            &RCC_ClkInitStruct,
+            FLASH_LATENCY_2) != HAL_OK)
+    {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
+
+/**
+  * @brief Scan all I2C addresses.
+  */
+void I2C_Scan(void)
+{
+    Logger_Print("I2C Scan Start\r\n");
+
+    for (uint8_t address = 1; address < 128; address++)
+    {
+        if (HAL_I2C_IsDeviceReady(
+                &hi2c1,
+                address << 1,
+                3,
+                100) == HAL_OK)
+        {
+            Logger_Printf(
+                "I2C Device Found: 0x%02X\r\n",
+                address
+            );
+        }
+    }
+
+    Logger_Print("I2C Scan Done\r\n");
+}
 
 /* USER CODE END 4 */
 
@@ -167,27 +261,21 @@ void SystemClock_Config(void)
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+    __disable_irq();
+
+    while (1)
+    {
+    }
 }
+
 #ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE BEGIN 6 */
+    (void)file;
+    (void)line;
+    /* USER CODE END 6 */
 }
+
 #endif /* USE_FULL_ASSERT */
